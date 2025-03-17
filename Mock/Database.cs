6cc2f4ef34de2036fda70ccb1ea4cd3a645f1de2
chani-klein <chani4166742@gmail.com>
2025-03-17 20:47:15 +0200

    change

--- Mock/Database.cs
+++ Mock/Database.cs
@@ -13,7 +13,7 @@ namespace Mock
     public class Database :DbContext ,IContext
     {
         public DbSet<Calls> Products { get; set; }
-        public DbSet<Category> Categories { get; set; }
+        public DbSet<ValueTa> Categories { get; set; }
         public DbSet<User> Users { get; set; }
 
         public void Save()
