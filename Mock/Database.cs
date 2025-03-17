4267820d9abfd749521e33f4811c32613c9049b2
RIVKI <rivkiasher@gmail.com>
2025-03-17 17:40:08 +0200

    Add project files.

---
+++ Mock/Database.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mock
+{
+    public class Database:DbContext ,IContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public void Save()
+        {
+           SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=sql;database=projectShopDb;trusted_connection=true;TrustServerCertificate=True");
+        }
+    }
+}
