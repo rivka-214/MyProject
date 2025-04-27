68c59a22c6903a800a8bfabb6468b57f89ccd13a
RIVKI <rivkiasher@gmail.com>
2025-04-27 23:50:31 +0300

    delete migration

--- Repository/Entities/Calls.cs
+++ Repository/Entities/Calls.cs
@@ -13,7 +13,7 @@ namespace Reposetory.Entities
     public class Calls
     {
         public int Id { get; set; }
-        public string? Region { get; set; }
+
         public double LocationX { get; set; }
         public double LocationY { get; set; }
         public string ImageUrl { get; set; }
@@ -21,21 +21,5 @@ namespace Reposetory.Entities
         public int? UrgencyLevel { get; set; }
         public string? Status { get; set; }
         public List<VolunteerCalls> VolunteerCalls { get; set; } = new List<VolunteerCalls>();
-        //public string InjuryType { get; set; }
-        //public int SeverityLevel { get; set; }
-        //public DateTime CallDateTime { get; set; }
-
     }
-
-    //public class FirstAidInstructions
-    //{
-    //    [Key]
-    //    public int Id { get; set; }
-
-    //    [Required]
-    //    public string InjuryType { get; set; }
-
-    //    [Required]
-    //    public string InstructionText { get; set; }
-    //}
 }
