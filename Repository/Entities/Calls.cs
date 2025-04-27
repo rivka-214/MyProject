57eb84061f37f0bbe6079bbb8295e05fed7487e0
RIVKI <rivkiasher@gmail.com>
2025-04-27 23:35:37 +0300

    fix

--- Repository/Entities/Calls.cs
+++ Repository/Entities/Calls.cs
@@ -13,13 +13,15 @@ namespace Reposetory.Entities
     public class Calls
     {
         public int Id { get; set; }
-        public string? Region { get; set; }
+        [Required]
         public double LocationX { get; set; }
+        [Required]
         public double LocationY { get; set; }
         public string ImageUrl { get; set; }
         public string? Description { get; set; }
         public int? UrgencyLevel { get; set; }
         public string? Status { get; set; }
+        public int numVolanteer { get; set; }
         public List<VolunteerCalls> VolunteerCalls { get; set; } = new List<VolunteerCalls>();
         //public string InjuryType { get; set; }
         //public int SeverityLevel { get; set; }
