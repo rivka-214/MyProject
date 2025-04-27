330daa86756b47b07f886288d6668b195c9df635
chani-klein <chani4166742@gmail.com>
2025-04-27 22:46:00 +0300

    marge

--- Repository/Entities/User.cs
+++ Repository/Entities/User.cs
@@ -9,7 +9,7 @@ namespace Repository.Entities
 {   
     public class User
     {
-       
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastNAme { get; set; }
