6b9c6fa69bab684ba22c2ffe9d361d8c527b714a
RIVKI <rivkiasher@gmail.com>
2025-04-28 23:45:11 +0300

    add

--- Repository/Entities/User.cs
+++ Repository/Entities/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Entities
 {
-    internal class User
+    public class User
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
