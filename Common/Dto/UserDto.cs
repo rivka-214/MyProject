2cc17f59c10e3e63d27d211d68328734f8d04559
RIVKI <rivkiasher@gmail.com>
2025-05-11 02:51:45 +0300

    a lot of change

--- Common/Dto/UserDto.cs
+++ Common/Dto/UserDto.cs
@@ -8,10 +8,12 @@ namespace Common.Dto
 {
     public class UserDto
     {
-        public int Id { get; set; }
+
+        public int Id { get; set; } // מפתח ראשי
         public string FirstName { get; set; }
         public string LastNAme { get; set; }
         public string PhoneNumber { get; set; }
         public string Gmail { get; set; }
+        public string password { get; set; }
     }
 }
