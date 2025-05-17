64c076d6f3fcf02e22af273ea46de811d45749d7
chani-klein <chani4166742@gmail.com>
2025-05-17 18:29:26 +0300

    17/05

---
+++ Mock/Migrations/20250512200839_init.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mock.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Region",
+                table: "VolunteersDb",
+                newName: "Address");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "VolunteersDb",
+                newName: "Region");
+        }
+    }
+}
